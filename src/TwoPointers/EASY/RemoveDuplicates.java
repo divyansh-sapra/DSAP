8d359d3a8886515fbd642b52f28d53a349eb06a3
Divyansh Sapra <you@example.com>
2025-12-18 01:58:17 +0530

    remove duplicates using slow and fast pointers

--- src/TwoPointers/EASY/RemoveDuplicates.java
+++ src/TwoPointers/EASY/RemoveDuplicates.java
@@ -1,40 +1,19 @@
 package TwoPointers.EASY;
 
-import java.util.Arrays;
-
 public class RemoveDuplicates {
     public static void main(String[] args) {
         char[] arr = {'1', '1', '2', '2', '3', '4', '4'};
         int len = arr.length;
         int i = 0;
-        int j = i+1;
-        int res = 1;
-        if (len == 1) {
-            System.out.println(1);
-            return;
-        }
+        int j = i + 1;
+        //slow and fast pointer
         while (j < len) {
-            if(arr[i] == arr[j]) {
-                arr[j] = '_';
-            } else if (arr[i] != arr[j]) {
-                res++;
-                i=j;
-            }
-            j++;
-        }
-        i=0;
-        j=0;
-        while (j < len) {
-            if (arr[i] == '_' && arr[j] != '_') {
+            if (arr[i] != arr[j]) {
+                i++;
                 arr[i] = arr[j];
-                arr[j] = '_';
-                i++;
-            } else if (arr[i] != '_' && arr[j] != '_') {
-                i++;
             }
             j++;
         }
-        System.out.println(Arrays.toString(arr));
-        System.out.println(res);
+        System.out.println(i + 1);
     }
 }
